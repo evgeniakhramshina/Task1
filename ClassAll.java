68252499d9cd730f957bdf6261a7d6229dba88da
evgeniakhramshina <74365702+evgeniakhramshina@users.noreply.github.com>
2020-11-14 01:14:46 +0300

    Add files via upload

---
+++ ClassAll.java
@@ -0,0 +1,10 @@
+import java.io.*; 
+import classes.Class1;
+import classes.Class2;
+
+public class ClassAll{
+public static void main(String[] args)
+{
+Class1.showMessage(args);
+Class2.showMessage(args);
+}}
---
+++ classes/Class2.java
@@ -0,0 +1,7 @@
+package classes;
+import java.io.*; 
+public class Class2{
+public static void showMessage(String[] args)
+{for(int i=0; i<args.length; i++)
+System.out.println("Class2: args[" + i + "] : " + args[i]);
+}}
